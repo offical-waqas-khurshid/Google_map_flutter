c2dc013e079e0010f8687a7eaa320ffe6b19a36b
waqas <waqaskhurshidoffical@gmail.com>
2023-05-21 02:53:12 +0500

    add google map type, add google map controller, cameraPosition

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Completer<GoogleMapController> _controller =  Completer();
+  static const CameraPosition _cameraPosition = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.4746);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        onMapCreated: (GoogleMapController controller){
+          _controller.complete(controller);
+        },
+        initialCameraPosition: _cameraPosition,
+      ),
+    );
+  }
+}
